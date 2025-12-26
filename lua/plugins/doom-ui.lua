e0d8e6369faecedbf2dd277f1155b38bf5556445
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2025-12-26 13:46:34 +0300

    Create doom-ui.lua

---
+++ lua/plugins/doom-ui.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      preset = "helix",
+      icons = {
+        breadcrumb = "》",
+        separator = "➜",
+        group = "+",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = { event = "msg_show", kind = "" },
+          opts = { skip = true },
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      },
+    },
+  },
+}
