ad18bbd56596f0d78f3928f9a618b192ab5164a6
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2025-12-26 13:52:35 +0300

    Update doom-ui.lua

--- lua/plugins/doom-ui.lua
+++ lua/plugins/doom-ui.lua
@@ -20,13 +20,6 @@ return {
     event = "VeryLazy",
     dependencies = { "MunifTanjim/nui.nvim" },
     opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
       routes = {
         {
           filter = { event = "msg_show", kind = "" },
