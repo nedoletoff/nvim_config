bd233abb7a713b0a9816d5497231454f1fc7b038
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2025-12-26 15:52:58 +0300

    Update bad-apple.lua
    Add libcanberra sound support configuration for bad-apple plugin

--- lua/plugins/bad-apple.lua
+++ lua/plugins/bad-apple.lua
@@ -1,9 +1,15 @@
 return {
-  {
-    "seandewar/bad-apple.nvim",
-    cmd = "BadApple",
-    config = function()
-      vim.keymap.set("n", "<leader>ba", "<cmd>BadApple<cr>", { noremap = true, silent = true })
-    end,
-  },
+  "seandewar/bad-apple.nvim",
+  cmd = "BadApple",
+  config = function()
+    vim.keymap.set("n", "<leader>ba", "<cmd>BadApple<cr>", { noremap = true, silent = true })
+    
+    -- Configure libcanberra sound support
+    -- Path to libcanberra.so library (works without X11)
+    -- Find the path with: find /usr -name "libcanberra.so*" 2>/dev/null
+    -- Common paths:
+    -- - /usr/lib/x86_64-linux-gnu/libcanberra.so.0
+    -- - /usr/lib/libcanberra.so
+    require("bad-apple.sound").libcanberra_fname = "/usr/lib/x86_64-linux-gnu/libcanberra.so.0"
+  end,
 }
