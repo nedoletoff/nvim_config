de8440d64107ec3f526cf639a0d1788cfa2534da
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2026-01-30 17:40:35 +0300

    Add user@host to statusline via polish.lua for doom.nvim

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,13 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- Add user@host to statusline
+local user = vim.loop.os_getenv("USER") or vim.loop.os_getenv("USERNAME") or "user"
+local host = vim.loop.os_gethostname() or "host"
+local user_host = user .. "@" .. host
+vim.g.user_host = user_host
 
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- Append to doom statusline right section after setup
+vim.defer_fn(function()
+  local ok, doom_statusline = pcall(require, "doom.modules.ui.statusline")
+  if ok and doom_statusline and doom_statusline.section and doom_statusline.section.rz then
+    table.insert(doom_statusline.section.rz, user_host)
+  end
+end, 200)
