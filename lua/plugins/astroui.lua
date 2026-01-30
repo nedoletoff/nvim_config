2fe16a734e932a4e2fb9eb14bc31de494f16af0c
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2026-01-30 17:45:32 +0300

    Enable astroui and add user@host to statusline

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,10 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
@@ -12,15 +5,37 @@ return {
   opts = {
     -- change colorscheme
     colorscheme = "astrodark",
+
+    -- configure AstroNvim status line with user@host
+    status = {
+      attributes = {
+        statusline = {}, -- statusline default attributes
+      },
+      component = {
+        -- add user@host component
+        user_host = function()
+          local user = vim.loop.os_getenv("USER") or vim.loop.os_getenv("USERNAME") or "user"
+          local host = vim.loop.os_gethostname() or "host"
+          return user .. "@" .. host
+        end,
+      },
+      section = {
+        rz = { " ", "user_host" }, -- add user_host to right section
+      },
+    },
+
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
-      init = { -- this table overrides highlights in all themes
+      init = {
+        -- this table overrides highlights in all themes
         -- Normal = { bg = "#000000" },
       },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+      astrodark = {
+        -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
       },
     },
+
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
