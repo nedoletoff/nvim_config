3c2dcc5fdbb035568270b58ac829609f6e29febb
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2026-05-19 11:40:35 +0300

    Enhance Mason setup with more tools
    Updated Mason configuration to include additional formatters, linters, and debuggers.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,33 @@
--- Customize Mason
-
----@type LazySpec
+-- lua/plugins/mason.lua
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "lua-language-server",
-      "stylua",
-      -- Python tools
-"pyright",      "black",
-      "ruff",
-      "debugpy",
-      -- Tree-sitter
-      "tree-sitter-cli",
-    },
+  -- Форматеры и линтеры
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "black", "isort",              -- Python
+        "jq",                          -- JSON
+        "hadolint",                    -- Docker
+        "shellcheck", "shfmt",         -- Bash
+        "golangci-lint", "gofumpt",    -- Go
+        "google-java-format",          -- Java
+        "cpplint",                     -- C++
+        "prettier", "eslint_d",        -- JS/TS
+      })
+    end,
+  },
+  -- Дебаггеры
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "python",                      -- debugpy
+        "bash",                        -- bash-debug-adapter
+        "go",                          -- delve
+        "javadbg", "javatest",         -- Java
+        "codelldb",                    -- C++
+        "js",                          -- js-debug-adapter
+      })
+    end,
   },
 }
