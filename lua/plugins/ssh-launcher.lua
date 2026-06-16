a75e7483dd4e55708025d043bd959932b61f4b8f
Никита Лучкин <48565773+nedoletoff@users.noreply.github.com>
2026-06-16 10:34:55 +0300

    feat: add leader keymaps and lazy cmd loading for ssh-launcher

--- lua/plugins/ssh-launcher.lua
+++ lua/plugins/ssh-launcher.lua
@@ -2,6 +2,12 @@ return {
   -- SSH-соединения: управление, добавление, редактирование, удаление
   {
     "G00380316/ssh-launcher.nvim",
+    cmd = { "SshLauncher", "SshAddKey", "SshEditKey" },
+    keys = {
+      { "<leader>ss", "<cmd>SshLauncher<cr>",  desc = "SSH: открыть соединение" },
+      { "<leader>sa", "<cmd>SshAddKey<cr>",    desc = "SSH: добавить ключ в agent" },
+      { "<leader>se", "<cmd>SshEditKey<cr>",   desc = "SSH: редактировать/удалить соединение" },
+    },
     config = function()
       require("ssh-launcher").setup({})
     end,
